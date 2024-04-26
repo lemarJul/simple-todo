2bb5c20dba44c29b97d3fc2bb761a3181cff2133
Julien Lemarchand <lemar.jul@gmail.com>
2024-04-26 15:42:11 +0200

    refactor: move logic to new file TodoCard.mjs

--- script.js
+++ script.js
@@ -1,53 +1,10 @@
-import Todo from "./components/Todo.mjs";
+import TodoCard from "./components/TodoCard.mjs";
 
 const form = document.getElementById("todoForm");
 const { todoList, todoProgress } = form.children;
 
-form.addEventListener("submit", function submitHandler(e) {
-  e.preventDefault();
-  if (todoContent()) todoList.createTodo(todoContent());
-  form.reset();
+const { todoList: myList } = TodoCard(form, todoList, todoProgress);
 
-  /**
-   * Retrieves the trimmed value of the todoInput element from the event target.
-   * @returns {string} - The trimmed value of the todoInput element.
-   */
-  function todoContent() {
-    return form.todoInput.value.trim();
-  }
-});
-
-todoList.createTodo = function createTodo(content, checked = false) {
-  this.appendChild(Todo(content, checked)).addEventListener("click", () =>
-    todoProgress.update()
-  );
-  todoProgress.update();
-};
-
-/**
- * Updates the progress text.
- * @returns {void}
- */
-todoProgress.update = () => {
-  const [checked, total] = countCheckboxProgression(todoList);
-  todoProgress.innerText = total ? format(checked, total) : "";
-};
-
-/**
- * Returns the number of checked checkboxes and the total checkboxes.
- * @param {HTMLElement} element - The element containing the checkboxes.
- * @returns {[number,number]} - An array containing the number of checked ones and the total.
- */
-function countCheckboxProgression(element) {
-  const checkboxes = element.querySelectorAll("input[type=checkbox]");
-  const checkedOnes = [...checkboxes].filter((item) => item.checked);
-  return [checkedOnes, checkboxes].map((item) => item.length);
-}
-
-function format(count, total) {
-  return `${count}/${total}`;
-}
-
-todoList.createTodo("Build a simple todo app", true);
-todoList.createTodo("Squeeze the lemon until it's dry");
-todoList.createTodo("Take a break");
+myList.createTodo("Build a simple todo app", true);
+myList.createTodo("Squeeze the lemon until it's dry");
+myList.createTodo("Take a break");
