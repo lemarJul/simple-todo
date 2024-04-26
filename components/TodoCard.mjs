7b262bb24a50e788d780775311669802cc48d852
Julien Lemarchand <lemar.jul@gmail.com>
2024-04-26 16:02:55 +0200

    refactor: enrich method of todoProgress assigned via a function

--- components/TodoCard.mjs
+++ components/TodoCard.mjs
@@ -1,51 +1,54 @@
 import Todo from "./Todo.mjs";
 
-export default function TodoCard(form,todoList,todoProgress){
+export default function TodoCard(form, todoList, todoProgress) {
+  enrichProgress(todoProgress);
 
-    form.addEventListener("submit", function submitHandler(e) {
-        e.preventDefault();
-        if (todoContent()) todoList.createTodo(todoContent());
-        form.reset();
-      
-        /**
-         * Retrieves the trimmed value of the todoInput element from the event target.
-         * @returns {string} - The trimmed value of the todoInput element.
-         */
-        function todoContent() {
-          return form.todoInput.value.trim();
-        }
-      });
-      
-      todoList.createTodo = function createTodo(content, checked = false) {
-        this.appendChild(Todo(content, checked)).addEventListener("click", () =>
-          todoProgress.update()
-        );
-        todoProgress.update();
-      };
-      
-      /**
-       * Updates the progress text.
-       * @returns {void}
-       */
-      todoProgress.update = () => {
-        const [checked, total] = countCheckboxProgression(todoList);
-        todoProgress.innerText = total ? format(checked, total) : "";
-      };
-      
-      /**
-       * Returns the number of checked checkboxes and the total checkboxes.
-       * @param {HTMLElement} element - The element containing the checkboxes.
-       * @returns {[number,number]} - An array containing the number of checked ones and the total.
-       */
-      function countCheckboxProgression(element) {
-        const checkboxes = element.querySelectorAll("input[type=checkbox]");
-        const checkedOnes = [...checkboxes].filter((item) => item.checked);
-        return [checkedOnes, checkboxes].map((item) => item.length);
-      }
-      
-      function format(count, total) {
-        return `${count}/${total}`;
-      }
+  form.addEventListener("submit", function submitHandler(e) {
+    e.preventDefault();
+    if (todoContent()) todoList.createTodo(todoContent());
+    form.reset();
 
-      return {form, todoList,todoProgress}
+    /**
+     * Retrieves the trimmed value of the todoInput element from the event target.
+     * @returns {string} - The trimmed value of the todoInput element.
+     */
+    function todoContent() {
+      return form.todoInput.value.trim();
+    }
+  });
+
+  todoList.createTodo = function createTodo(content, checked = false) {
+    this.appendChild(Todo(content, checked)).addEventListener("click", () =>
+      todoProgress.assess(todoList)
+    );
+    todoProgress.assess(todoList);
+  };
+  return { form, todoList, todoProgress };
+}
+
+
+function enrichProgress(progressElement) {
+  return Object.assign(progressElement, {
+    /**
+     * Updates the progress text.
+     * @returns {void}
+     */
+    assess: function assess(assessed) {
+      const [checked, total] = countCheckboxProgression(assessed);
+      progressElement.innerText = total ? format(checked, total) : "";
+    },
+  });
+}
+/**
+ * Returns the number of checked checkboxes and the total checkboxes.
+ * @param {HTMLElement} element - The element containing the checkboxes.
+ * @returns {[number,number]} - An array containing the number of checked ones and the total.
+ */
+function countCheckboxProgression(element) {
+  const checkboxes = element.querySelectorAll("input[type=checkbox]");
+  const checkedOnes = [...checkboxes].filter((item) => item.checked);
+  return [checkedOnes, checkboxes].map((item) => item.length);
+}
+function format(count, total) {
+  return `${count}/${total}`;
 }
